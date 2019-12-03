37374437aaaa664d9370f3a62e5d6f8ca126d098
cantasci <cantasci@live.com>
2019-12-03 23:01:36 +0300

    Get movie titles - hackerrank

--- nodejs/getMoviesTitle.js
+++ nodejs/getMoviesTitle.js
@@ -8,76 +8,75 @@
 const https = require('https')
 const log = console.log
 
+const SEARCH_URL = `https://jsonmock.hackerrank.com/api/movies/search`;
+
 const getTotalPages = (url, callback) => {
   https.get(url, res => {
-    let data = ''
+    let data = '';
 
-    res.on('data', chunk => data += chunk)
+    res.on('data', chunk => data += chunk);
 
     res.on('end', () => {
-      return callback(null, JSON.parse(data).total_pages)
+      return callback(null, JSON.parse(data).total_pages);
     })
   }).on('error', err => {
-    return callback(err, null)
+    return callback(err, null);
   })
 }
 
 const getMovieTitlesHelper = (urls, callback) => {
-  let titles = new Array()
-  let counter = 1
+  let titles = new Array();
+  let counter = 1;
 
   urls.forEach(url => {
     https.get(url, res => {
       let data = ''
-
-      res.on('data', chunk => data += chunk)
-
+      res.on('data', chunk => data += chunk);
       res.on('end', () => {
-        const movies = JSON.parse(data).data
+        const movies = JSON.parse(data).data;
 
-        movies.forEach(movie => titles.push(movie.Title))
+        titles = [
+          ...titles,
+          ...movies.map(movie => movie.Title),
+        ];
 
         if (counter === urls.length) {
-          return callback(null, titles)
+          return callback(null, titles);
         }
-        counter++
+        counter += 1;
       })
     }).on('error', err => {
-      return callback(err, null)
+      return callback(err, null);
     })
   })
 }
 
-/*
- * Complete the function below.
- * Use console.log to print the result, you should not return from the function.
- */
 const getMovieTitles = substr => {
   if (substr === '') {
-    return null
+    return null;
   }
 
-  const url = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`
+  const url = `${SEARCH_URL}/?Title=${substr}`;
   getTotalPages(url, (err, totalPages) => {
     if (err) {
-      log('Error getting total number of available pages:', err)
+      log('Error getting total number of available pages:', err);
     } else {
       const urls = new Array()
       for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
-        urls.push(`${url}&page=${pageNumber}`)
+        urls.push(`${url}&page=${pageNumber}`);
       }
       getMovieTitlesHelper(urls, (err, titles) => {
         if (err) {
-          log('Error retrieving movies titles:', err)
+          log('Error retrieving movies titles:', err);
         } else {
-          titles.sort().forEach(title => log(title))
+          titles.sort().forEach(title => log(title));
         }
       })
     }
   })
 }
 
-getMovieTitles('spiderman')
+getMovieTitles('spiderman');
 /* =>
 Amazing Spiderman Syndrome
 Fighting, Flying and Driving: The Stunts of Spiderman 3
