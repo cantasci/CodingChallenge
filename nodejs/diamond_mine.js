2eb0320571e8db96b2ceb5f0d835a1066867fd9f
cantasci <cantasci@live.com>
2019-12-07 21:07:26 +0300

    Add a solution for collecting diamonds

---
+++ nodejs/diamond_mine.js
@@ -0,0 +1,217 @@
+/**
+ *
+ * Collect maximum diamonds
+ *
+ * @param {array} mat
+ */
+function collectMax(mat) {
+  const MAX = 1000;
+  const LEFT = 'left';
+  const RIGHT = 'right';
+  const UP = 'up';
+  const DOWN = 'down';
+
+  const ROW_COUNT = mat.length;
+  if (ROW_COUNT == 0) {
+    return 0;
+  }
+
+  const COLUMN_COUNT = mat[0].length;
+  if (COLUMN_COUNT == 0) {
+    return 0;
+  }
+
+  let row = 0;
+  let column = 0;
+  let diamonds = 0;
+  let direction = RIGHT;
+  let loopCount = -1;
+
+  /**
+   * Get next position
+   *
+   * @param {*} direction
+   * @param {*} row
+   * @param {*} column
+   * @param {*} max
+   */
+  const getNextPosition = (direction, row, column, max) => {
+    switch (direction) {
+      case LEFT:
+        column = Math.max(0, column - 1);
+        break;
+        break;
+      case RIGHT:
+        column = Math.min(max - 1, column + 1);
+        break;
+      case UP:
+        row = Math.max(0, row - 1);
+        break;
+      case DOWN:
+        row = Math.min(max - 1, row + 1);
+        break;
+    }
+
+    return { newRow: row, newColumn: column };
+  };
+
+  /**
+   *
+   * Get previous position
+   *
+   * @param {string} direction
+   * @param {number} row
+   * @param {string} column
+   * @param {number} max
+   */
+  const getPreviousPosition = (direction, row, column, max) => {
+    switch (direction) {
+      case LEFT:
+        column = Math.min(max - 1, column + 1);
+        break;
+        break;
+      case RIGHT:
+        column = Math.max(0, column - 1);
+        break;
+      case UP:
+        row = Math.min(max - 1, row + 1);
+        break;
+      case DOWN:
+        row = Math.max(0, row - 1);
+        break;
+    }
+
+    return { newRow: row, newColumn: column };
+  };
+
+  /**
+   *
+   * Check the row and the column to reach to starting point
+   *
+   * @param {number} row
+   * @param {number} column
+   * @param {string} direction
+   */
+  const isReachedStartPoint = (row, column, direction) => {
+    return [LEFT, UP].indexOf(direction) > -1 && row == 0 && column === 0;
+  };
+
+  /**
+   *
+   * Get possible new direction
+   *
+   * @param {array} mat
+   * @param {number} row
+   * @param {number} column
+   */
+  const getPossibleNewDirection = (mat, row, column) => {
+    const UNDEFINED_VALUE = -2;
+    const right =
+        column == ROW_COUNT - 1 ? UNDEFINED_VALUE : mat[row][column + 1],
+      left = column == 0 ? UNDEFINED_VALUE : mat[row][column - 1],
+      up = row == 0 ? UNDEFINED_VALUE : mat[row - 1][column],
+      down = row == COLUMN_COUNT - 1 ? UNDEFINED_VALUE : mat[row + 1][column];
+
+    let direction = null;
+    let lookingValue = 1;
+    if (right === lookingValue) {
+      direction = RIGHT;
+    } else if (down === lookingValue) {
+      direction = DOWN;
+    } else if (left === lookingValue) {
+      direction = LEFT;
+    } else if (up === lookingValue) {
+      direction = UP;
+    } else {
+      lookingValue = 0;
+      if (right === lookingValue) {
+        direction = RIGHT;
+      } else if (down === lookingValue) {
+        direction = DOWN;
+      } else if (left === lookingValue) {
+        direction = LEFT;
+      } else if (up === lookingValue) {
+        direction = UP;
+      }
+    }
+    return direction;
+  };
+
+  while (++loopCount < MAX) {
+    // console.log(`row:${row} - column:${column} - direction:${direction} - mat:${mat}`);
+    if (isReachedStartPoint(row, column, direction)) {
+      break;
+    }
+
+    const current = mat[row][column];
+    if (current === 1) {
+      // collect the diamond
+      diamonds += 1;
+      mat[row][column] = 0;
+      continue;
+    } else if (current === 0) {
+      // move
+      const { newRow, newColumn } = getNextPosition(
+        direction,
+        row,
+        column,
+        ROW_COUNT
+      );
+      if (row == newRow && column == newColumn) {
+        const newDirection = getPossibleNewDirection(mat, row, column);
+        if (newDirection) {
+          direction = newDirection;
+        } else {
+          diamonds = 0;
+          loopCount = MAX + 1;
+        }
+      } else {
+        row = newRow;
+        column = newColumn;
+      }
+    } else if (current === -1) {
+      // reached the block and go to different direction
+      const { newRow, newColumn } = getPreviousPosition(
+        direction,
+        row,
+        column,
+        ROW_COUNT
+      );
+      row = newRow;
+      column = newColumn;
+
+      const newDirection = getPossibleNewDirection(mat, row, column);
+      if (newDirection) {
+        direction = newDirection;
+      } else {
+        diamonds = 0;
+        loopCount = MAX + 1;
+      }
+    }
+  }
+
+  return diamonds;
+}
+
+// test cases
+console.log(
+  collectMax([
+    [0, 1, -1],
+    [1, 0, -1],
+    [1, 1, 1],
+  ]) === 5
+);
+console.log(
+  collectMax([
+    [0, 1, -1],
+    [1, 0, -1],
+    [-1, -1, -1],
+  ]) === 2
+);
+console.log(
+  collectMax([
+    [-1, 1, -1],
+    [-1, 0, -1],
+    [-1, -1, -1],
+  ]) === 0
+);
